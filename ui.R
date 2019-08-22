0451abb5ee267e38a8541570b57d206abd4a37b1
Jeremy Dudek <jeremy@doctilio.com>
2019-08-22 17:44:12 +0200

    [Modification] Deuxième modification

--- ui.R
+++ ui.R
@@ -14,7 +14,7 @@ shinyUI(fluidPage(
 
     # Application title
     titlePanel("Test déploiement Travis - ShinyApps.io"),
-
+    shiny::tags$p("Ce déploiement est entièrement réalisé par un job Travis !"),
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
